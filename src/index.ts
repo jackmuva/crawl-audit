044e9085a22b52b2d0b92b89baa15698809a57b8
jackmuva <jackmu@umich.edu>
2026-03-05 13:55:43 -0800

    have an agent mode that goes through each url

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import figlet from "figlet";
 import fs from "fs";
 import { crawlAndWatch } from "./lib/crawl";
-import { searchByText } from "./lib/analyze";
+import { AiPrompt, searchByText } from "./lib/analyze";
 
 const main = async () => {
 	const welcomeMessage = figlet.textSync('Crawl Audit!');
@@ -12,7 +12,7 @@ const main = async () => {
 
 	setupDirectoryEnv(domainInput);
 
-	let modeInput = prompt("Would you like to trigger a new crawl (0) or analyze the pages from last crawl (1) \n[Type either 0 or 1]:");
+	let modeInput = prompt("Would you like to trigger a new crawl [0] or analyze the pages from last crawl [1] [Type either 0 or 1]:");
 
 	while (true) {
 		if (modeInput === "0") {
@@ -20,19 +20,29 @@ const main = async () => {
 			break;
 		} else if (modeInput === "1") {
 			while (true) {
-				let analyzeInput = prompt("Would you like to perform a string match [0] or an agent search [1] \n[Type either 0 or 1]:");
+				let analyzeInput = prompt("Would you like to perform a string match [0] or an agent command [1] [Type either 0 or 1]:");
+				let workingDirectory = "./markdown_files/" + domainInput;
+				let mostRecentDate: string = "";
+				for (const file of fs.readdirSync(workingDirectory)) {
+					if (!mostRecentDate) mostRecentDate = file;
+					if (new Date(mostRecentDate) > new Date(file)) mostRecentDate = file;
+				}
+				workingDirectory += "/" + mostRecentDate;
+
 				if (analyzeInput === "0") {
-					let workingDirectory = "./markdown_files/" + domainInput;
-					let mostRecentDate: string = "";
-					for (const file of fs.readdirSync(workingDirectory)) {
-						if (!mostRecentDate) mostRecentDate = file;
-						if (new Date(mostRecentDate) > new Date(file)) mostRecentDate = file;
-					}
-					workingDirectory += "/" + mostRecentDate;
 					const searchInput = prompt("search string: ");
-					searchByText(searchInput ?? "", workingDirectory);
+					await searchByText(searchInput ?? "", workingDirectory);
 					break;
 				} else if (analyzeInput === "1") {
+					let promptInput = prompt("Agent prompt: ");
+					while (true) {
+						if (promptInput) {
+							await AiPrompt(promptInput, workingDirectory);
+							break;
+						} else {
+							promptInput = prompt("Enter a prompt for the agent: ");
+						}
+					}
 					break;
 				} else {
 					analyzeInput = prompt("Invalid input. Type either 0 or 1: ");
--- src/lib/analyze.ts
+++ src/lib/analyze.ts
@@ -1,3 +1,4 @@
+import { generateText, type ModelMessage } from "ai";
 import { $ } from "bun";
 
 
@@ -5,7 +6,6 @@ export const searchByText = async (text: string, dir: string) => {
 	$.cwd(dir);
 	let reportName = prompt("Provide a name for your report: ");
 	if (!reportName) reportName = "report_" + (new Date()).toISOString()
-
 
 	const file = Bun.file("./reports/" + reportName + ".md");
 	const writer = file.writer();
@@ -23,6 +23,51 @@ export const searchByText = async (text: string, dir: string) => {
 			await writer.flush();
 			urlCache[url] = true;
 		}
+	}
+
+	console.log("Finished writing report to reports/" + reportName + ".md");
+}
+
+export const AiPrompt = async (sysPrompt: string, dir: string) => {
+	$.cwd(dir);
+	let reportName = prompt("Provide a name for your report: ");
+	if (!reportName) reportName = "report_" + (new Date()).toISOString()
+
+	const file = Bun.file("./reports/" + reportName + ".md");
+	const writer = file.writer();
+
+	await writer.write(`User prompt: ${sysPrompt}\n\n`);
+	await writer.flush();
+
+	const files = await $`ls`.text();
+	const urlCache: { [url: string]: boolean } = {};
+	for (const line of files.split("\n")) {
+		const url = line.split(".md")[0]?.replaceAll("_", "/");
+		if (url && !(url in urlCache)) {
+			const file = Bun.file(dir + "/" + line);
+
+			const messages: ModelMessage[] = [];
+			messages.push({
+				role: "system",
+				content: sysPrompt,
+			});
+			messages.push({
+				role: "user",
+				content: "Analyze this content:\n" + (await file.text()),
+			});
+
+			const { text } = await generateText({
+				model: "anthropic/claude-haiku-4.5",
+				messages: messages,
+			});
+
+			console.log("Agent analyzed " + url);
+			writer.write("[" + url + "]\n");
+			writer.write(text + "\n\n");
+			writer.flush();
+			urlCache[url] = true;
+		}
+		break;
 	}
 
 	console.log("Finished writing report to reports/" + reportName + ".md");
